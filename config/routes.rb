bc60ccca71bd86ce5e5b67ba3868e64a932aa47a
Loren Glenn <lorencglenn@gmail.com>
2017-02-17 09:40:37 -0800

    add functionality on dish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-
-  get 'reviews/edit'
 
   devise_for :users
-  resources :dishes
+  resources :dishes do
+    resources :reviews
+  end
   root 'dishes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
