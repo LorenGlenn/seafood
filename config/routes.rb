69a674ced4d9806decbc39f87cc0f6464400b420
Loren Glenn <lorencglenn@gmail.com>
2017-02-17 08:12:13 -0800

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'dishes/index'
 
   get 'dishes/show'
--- db/migrate/20170217160203_create_dishes.rb
+++ db/migrate/20170217160203_create_dishes.rb
@@ -1,7 +1,10 @@
 class CreateDishes < ActiveRecord::Migration[5.0]
   def change
     create_table :dishes do |t|
-
+      t.string :name
+      t.integer :cost
+      t.string :image
+      t.integer :rating
       t.timestamps
     end
   end
