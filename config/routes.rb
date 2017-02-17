0704b233f99b4bdc1d73634c0749281dd81dcb46
Loren Glenn <lorencglenn@gmail.com>
2017-02-17 08:02:41 -0800

    create dish model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'dishes/index'
+
+  get 'dishes/show'
+
+  get 'dishes/edit'
+
+  get 'dishes/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
