c09e5c8f6ee7e23fea86b443c270df62e25518e8
Loren Glenn <lorencglenn@gmail.com>
2017-02-17 09:26:50 -0800

    add crud for dish object

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,13 +1,51 @@
 class DishesController < ApplicationController
   def index
-  end
+  @dishes = Dish.all
+end
 
-  def show
-  end
+def show
+  @dish = Dish.find(params[:id])
+end
 
-  def edit
-  end
+def new
+  @dish = Dish.new
+end
 
-  def new
+def create
+  @dish = Dish.new(dish_params)
+  if @dish.save
+    flash[:notice] = "Dish succesfully added!"
+    redirect_to dishes_path
+  else
+    flash[:notice] = "Dish not added!"
+    render :new
   end
+end
+
+def edit
+  @dish = Dish.find(params[:id])
+end
+
+def update
+  @dish = Dish.find(params[:id])
+  if @dish.update(dish_params)
+    flash[:notice] = "Dish Updated!"
+    redirect_to dishes_path
+  else
+    flash[:alert] = "Dish Update Failed!"
+    redirect_to dishes_path
+  end
+end
+
+def destroy
+  @dish = Dish.find(params[:id])
+  @dish.destroy
+  flash[:notice] = "Dish Deleted!"
+  redirect_to dishes_path
+end
+
+private
+def dish_params
+  params.require(:dish).permit(:name, :cost, :country, :image)
+end
 end
