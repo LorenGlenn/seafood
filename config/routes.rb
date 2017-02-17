8a35b4935a30fd9f9745213e9a07cddfaa06a080
Loren Glenn <lorencglenn@gmail.com>
2017-02-17 09:18:50 -0800

    refactor login, working login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/edit'
+
   devise_for :users
   resources :dishes
+  root 'dishes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217165247) do
+ActiveRecord::Schema.define(version: 20170217170049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20170217165247) do
     t.integer  "cost"
     t.string   "image"
     t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "score"
+    t.integer  "dish_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
