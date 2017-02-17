1fb96b63d1b6dd2d26b64a0549f656510d6b7ca3
Loren Glenn <lorencglenn@gmail.com>
2017-02-17 08:32:45 -0800

    setup admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'dishes/index'
-
-  get 'dishes/show'
-
-  get 'dishes/edit'
-
-  get 'dishes/new'
+  resources :dishes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
